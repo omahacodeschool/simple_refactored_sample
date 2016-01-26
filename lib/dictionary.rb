6089e198bdb6a18e60f62f0485235018db57be95
Beth Haubert <bethany.haubert@gmail.com>
2016-01-26 14:07:41 -0600

    dictionary test passing

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,8 +1,7 @@
 def valid_word?(word)
   # Populate this array with some words of your choosing.
   dictionary = ["cat", "cats","running", "run", "trance", "publish", "pub"]
-  	dictionary.loop do 
-  		
+  dictionary.include?(word)
   # Return true if your dictionary contains `word`
   # or false if it does not.
 end
