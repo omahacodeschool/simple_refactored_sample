452bbc4f6266a18638bb709ee9165d187e93c1c0
Sumeet Jain <sumeet@sumeetjain.com>
2016-01-28 15:43:50 -0600

    Conversion to OOP

--- lib/string_segmenter.rb
+++ lib/string_segmenter.rb
@@ -1,81 +1,95 @@
 require_relative '../lib/dictionary.rb'
 
-def segment_string(str)
-  words = []
+class Segment
+	def initialize(str)
+		@str = str
+		@words = []
+		@word_index = Hash.new(0)
+		@first_letter = 0
+		@last_letter = 0
+	end
 
-  word_index = Hash.new(0)
+	def final_words
+		while @last_letter <= @str.length do  
+	    if danglers?
+	      @last_letter = index_of_most_recently_found_word + 1
 
-  first_letter= 0
-  last_letter = 0
-  
-  while last_letter <= str.length do  
-    part_of_word = str[first_letter..last_letter]
+	      @words.delete(most_recently_found_word)
 
-    if danglers?(str, first_letter, last_letter, words)
-      last_letter = index_of_most_recently_found_word(word_index) + 1
+	      @word_index.delete(most_recently_found_word)
 
-      words.delete(most_recently_found_word(word_index))
+	      @first_letter = index_of_most_recently_found_word + 1 
 
-      word_index.delete(most_recently_found_word(word_index))
+	    elsif !valid_word?(part_of_word)
+	      @last_letter += 1
 
-      if index_of_most_recently_found_word(word_index) == nil
-        first_letter = 0
-      else 
-        first_letter = index_of_most_recently_found_word(word_index) + 1 
-      end
+	    else
+	    	store_valid_word(part_of_word)
 
-    elsif !valid_word?(part_of_word)
-      last_letter += 1
+	      @first_letter = @last_letter + 1
+	      @last_letter += 1
+	    end
+	  end
 
-    elsif valid_word?(part_of_word)
-      words << part_of_word
+  	return @words
+	end
 
-      word_index[part_of_word] = last_letter      
+	# Get a segment of the full string based on a starting and ending point.
+	# 
+	# Returns String.
+	def part_of_word
+		@str[@first_letter..@last_letter]
+	end
 
-      first_letter = last_letter + 1
-      last_letter += 1
-    end
-  end
+	# Adds a word into the running collection.
+	# 
+	# Returns True (not that it matters).
+	def store_valid_word(word)
+		@words << word
+		@word_index[word] = @last_letter
+	end
 
-  return words
-end
+	# Checks if there are danglers at the end of the string.
+	# 
+	# str - Entire String that's being segmented.
+	# first_letter - Integer index of the first letter from str that'll begin the section of str that's being checked.
+	# last_letter - Integer index of last letter from str.
+	# words - Array with words that have been identified so far.
+	# 
+	# Returns Boolean.
+	def danglers?
+	  !valid_word?(@str[@first_letter..@last_letter]) && @last_letter == @str.length && @words.join.length < @str.length
+	end
 
-# Checks if there are danglers at the end of the string.
-# 
-# str - Entire String that's being segmented.
-# first_letter - Integer index of the first letter from str that'll begin the section of str that's being checked.
-# last_letter - Integer index of last letter from str.
-# words - Array with words that have been identified so far.
-# 
-# Returns Boolean.
-def danglers?(str, first_letter, last_letter, words)
-  !valid_word?(str[first_letter..last_letter]) && last_letter == str.length && words.join.length < str.length
-end
+	# Get the list of words that have been found so far.
+	# 
+	# words_with_their_index - Hash with word as key and its position as value.
+	# 
+	# Returns Array of words.
+	def found_words
+	  @word_index.keys
+	end
 
-# Get the list of words that have been found so far.
-# 
-# words_with_their_index - Hash with word as key and its position as value.
-# 
-# Returns Array of words.
-def found_words(words_with_their_index)
-  words_with_their_index.keys
-end
+	# Get position of most recently found word.
+	# 
+	# words_with_their_index - Hash with word as key and its position as value.
+	# 
+	# Returns Integer.
+	def index_of_most_recently_found_word
+	  if @word_index.values.max == nil
+	  	return -1
+	  else
+	  	return @word_index.values.max
+	  end
+	end
 
-# Get position of most recently found word.
-# 
-# words_with_their_index - Hash with word as key and its position as value.
-# 
-# Returns Integer.
-def index_of_most_recently_found_word(words_with_their_index)
-  words_with_their_index.values.max
-end
-
-# Get the most recently added word.
-# 
-# words_with_their_index - Hash with word as key and its position as value.
-# 
-# Returns the word as a String.
-def most_recently_found_word(words_with_their_index)
-  i = index_of_most_recently_found_word(words_with_their_index)
-  words_with_their_index.key(i)
+	# Get the most recently added word.
+	# 
+	# words_with_their_index - Hash with word as key and its position as value.
+	# 
+	# Returns the word as a String.
+	def most_recently_found_word
+	  i = index_of_most_recently_found_word
+	  @word_index.key(i)
+	end
 end
--- tests/string_segmenter_test.rb
+++ tests/string_segmenter_test.rb
@@ -4,26 +4,30 @@ require 'string_segmenter'
 class StringSegmenterTest < Minitest::Test
   def test_ing_words
   	str = "catrunningtrance"
+    seg = Segment.new(str)
 
-  	assert_equal(["cat", "running", "trance"], segment_string(str))
+  	assert_equal(["cat", "running", "trance"], seg.final_words)
   end
 
   def test_plural_words
   	str = "catsrunningtrance"
+    seg = Segment.new(str)
 
-  	assert_equal(["cats", "running", "trance"], segment_string(str))
+  	assert_equal(["cats", "running", "trance"], seg.final_words)
   end
 
   def test_compoundish_words
   	str = "publishcatrun"
+    seg = Segment.new(str)
 
-  	assert_equal(["publish", "cat", "run"], segment_string(str))
+  	assert_equal(["publish", "cat", "run"], seg.final_words)
   end
   
-    def test_simple
+  def test_simple
   	str = "catrun"
+    seg = Segment.new(str)
 
-  	assert_equal(["cat", "run"], segment_string(str))
+  	assert_equal(["cat", "run"], seg.final_words)
   end
   
 end
